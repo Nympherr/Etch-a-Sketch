62488481b008708481cc9a21fdc5a289400cf864
Lukas S <114870613+Nympherr@users.noreply.github.com>
2023-02-14 02:38:22 +0200

    Add change color with mouse
    Add event on grid squares.
    Color changes when it is clicked
    and dragged with mouse. Only
    works with default color for now.
    Fixed bug when it showed invisible
    elements when scrolling with mouse.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 
 let x = 50;
 let y = 50;
+let clicked = false;
+let color = "green";
 
 let innerDiv = new Array(x);
 let outerDiv = new Array(y);
@@ -16,10 +18,26 @@ for(let i = 0; i < x ; i++){
     for(let j = 0; j < y; j++){
 
         innerDiv[j] = document.createElement("div");
-        innerDiv[j].classList.add("colored");
+
+        innerDiv[j].addEventListener("mousedown", function(){
+            this.style.backgroundColor = color;
+            clicked = true;
+        });
+        innerDiv[j].addEventListener("mouseover", function(){
+            if(clicked){
+                this.style.backgroundColor = color;
+            }
+        });
+        innerDiv[j].addEventListener("mouseup", function(){
+            clicked = false;
+        });
+
         innerDiv[j].style.cssText = " display:flex; flex-grow:1; flex-shrink:1;";
-        innerDiv[j].textContent = ".";
+        innerDiv[j].textContent = " ";
         outerDiv[i].appendChild(innerDiv[j]);
     }
+}
 
+function colorChange(){
+    this.classList.add("colored");
 }
